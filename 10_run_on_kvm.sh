71b571178d56286c818ae9f8356246dfe13bab5d
teou1 <teou1@users.noreply.github.com>
2025-12-25 00:33:19 +0100

    Update 10_run_on_kvm.sh

--- 10_run_on_kvm.sh
+++ 10_run_on_kvm.sh
@@ -4,7 +4,7 @@ qemu-system-x86_64 -m 1G -enable-kvm \
     -machine q35,smm=on \
     -drive if=pflash,format=raw,readonly=on,file=/usr/share/edk2/x64/OVMF_CODE.4m.fd \
     -drive if=pflash,format=raw,file=/usr/share/edk2/x64/OVMF_VARS.4m.fd \
-    -drive format=raw,file=vm/disk.img \
+    -drive format=raw,file=disk.img \
     -netdev user,id=net0 \
     -device e1000,netdev=net0 \
     -vga qxl \
