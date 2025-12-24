f30f0199d4b683932188262899e826b6663e8534
teou1 <teou1@users.noreply.github.com>
2025-12-25 00:20:54 +0100

    Update 01_get.sh
    versions update, alpine 3.23 and kernel 6.18

--- 01_get.sh
+++ 01_get.sh
@@ -1,20 +1,20 @@
 #/bin/bash
 
-alpineminirootfsfile="alpine-minirootfs-3.21.0-x86_64.tar.gz"
-linuxver="linux-6.10.14"
+alpineminirootfsfile="alpine-minirootfs-3.23.2-x86_64.tar.gz"
+linuxver="linux-6.18.2"
 
 cd ./build
 
 if [ ! -d alpine-minirootfs ]; then
     # Getting the Alpine minirootfs
-    wget -c4 http://dl-cdn.alpinelinux.org/alpine/v3.21/releases/x86_64/$alpineminirootfsfile
+    wget -c4 http://dl-cdn.alpinelinux.org/alpine/v3.23/releases/x86_64/$alpineminirootfsfile
     mkdir alpine-minirootfs
     tar -C ./alpine-minirootfs -xf $alpineminirootfsfile
 fi
 
 if [ ! -d linux ]; then
     # Getting the Linux kernel
-    wget -c4 http://cdn.kernel.org/pub/linux/kernel/v6.x/$linuxver.tar.xz
+    wget http://cdn.kernel.org/pub/linux/kernel/v6.x/$linuxver.tar.xz
     tar -xf $linuxver.tar.xz
     ln -s $linuxver linux
 fi
