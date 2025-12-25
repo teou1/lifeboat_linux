f92eca174de0a58406376180088fc1a670bcd92e
teou1 <teou1@users.noreply.github.com>
2025-12-25 18:12:34 +0100

    Update 02_chrootandinstall.sh
    removed vim, added partition tools

--- 02_chrootandinstall.sh
+++ 02_chrootandinstall.sh
@@ -14,7 +14,7 @@ apk add \
     openrc busybox-openrc busybox-mdev-openrc haveged \
     bash gawk grep sed util-linux unzip tar zstd mtools \
     font-terminus kbd \
-    nano vim \
+    nano net-tools pigz pixz partclone partclone-utils partimage \
     parted efibootmgr gptfdisk \
     lvm2 cryptsetup dmraid mdadm \
     e2fsprogs e2fsprogs-extra dosfstools xfsprogs btrfs-progs
@@ -22,10 +22,11 @@ apk add \
 apk add mandoc \
     util-linux-doc \
     parted-doc efibootmgr-doc dmraid-doc mdadm-doc lvm2-doc cryptsetup-doc gptfdisk-doc \
-    e2fsprogs-doc dosfstools-doc xfsprogs-doc btrfs-progs-doc
+    e2fsprogs-doc dosfstools-doc xfsprogs-doc btrfs-progs-doc net-tools-doc pigz-doc partclone-doc \
+    partclone-utils-doc partimage-doc
 
 rm /var/cache/apk/*
 
 # Make vim weight less on the final image
-rm -rf /usr/share/vim/*/doc/*
+# rm -rf /usr/share/vim/*/doc/*
 EOF
