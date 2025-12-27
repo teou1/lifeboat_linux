c7f54ad1281c2a0dacaedee1829766bea558439a
teou1 <teou1@users.noreply.github.com>
2025-12-27 21:18:47 +0100

    Update create_vm.sh

--- create_vm.sh
+++ create_vm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rm -f vm/disk.img
+rm -f vm/lifeboatx64.img
 mkdir -p vm/tmp/EFI/BOOT
 cp lifeboatx64.efi vm/tmp/EFI/BOOT/bootx64.efi
 virt-make-fs --format=raw --partition=gpt --type=vfat --label=ESP vm/tmp vm/lifeboat.img
